67a92fbf62f51344525b7819826f367150d8ef3e
sergioneto12 <sergio.ght14@gmail.com>
2020-10-10 15:58:24 -0300

    First Commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var $target = $('.anime'),
+    animationClass = 'anime-start',
+    offset = $(window).height() * 1/2;
+
+
+function animationScroll() {
+    var DocumentTop = $(document).scrollTop();
+    console.log(DocumentTop);
+
+    $target.each(function() {
+        var itemTop = $(this).offset().top;
+        if (DocumentTop > itemTop - offset) {
+            $(this).addClass(animationClass);
+        } else {
+            $(this).removeClass(animationClass);
+        }
+    });
+}
+
+$(document).scroll(function(){
+    animationScroll();
+})
